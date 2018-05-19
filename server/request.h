75d64e26d15789a6443158672dbcc260549bbf73
nujhedtozu <junior.tzou@gmail.com>
2018-05-18 20:56:25 -0400

    add request.h

---
+++ server/request.h
@@ -0,0 +1,11 @@
+/************************************************\
+ * Header file for request.c                 *
+\************************************************/
+
+#ifndef REQUEST_H
+#define REQUEST_H
+
+
+void request_handle(void *arg);
+
+#endif
